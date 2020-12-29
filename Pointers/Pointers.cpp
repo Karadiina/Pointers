e6ba3a0a9d14252b16f3cc1919efe90b3d939ce9
Karadiina <karadiina.ma@gmail.com>
2020-12-29 16:12:30 +0200

    .

--- Pointers/Pointers.cpp
+++ Pointers/Pointers.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 
+void manipulate(int value) {
+	value = 10;
+	std::cout << "value of int in manipulate: " << value << std::endl;
+}
+
 int main()
 {
 	int nValue = 8;
-	
+	int* pnValue = &nValue;
+
+	std::cout << "int value: " << nValue << std::endl;
+	std::cout << "pinter to int address: " << pnValue << std::endl;
+	std::cout << "======================== " << pnValue << std::endl;
+
+
 	return 0;
 }
